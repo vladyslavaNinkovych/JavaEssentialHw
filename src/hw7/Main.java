b97c7c911c8675aea72f4c0949e8d795c6ebb937
vninkovych <vninkovych@cogniance.com>
2020-09-05 11:43:57 +0300

    HW5

---
+++ src/hw7/Main.java
@@ -0,0 +1,11 @@
+package hw7;
+
+import hw3.task3.Vehicle;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicles vehicle = Vehicles.CAR;
+        System.out.println(vehicle.getColour());
+        System.out.println(vehicle.toString());
+    }
+}
---
+++ src/hw7/Vehicles.java
@@ -0,0 +1,38 @@
+package hw7;
+
+public enum Vehicles {
+    CAR(100, "yellow"), PLANE(200, "black"), TRAIN(400, "red"), BOAT(500, "green");
+
+    private int price;
+
+    Vehicles(int price, String colour) {
+        this.price = price;
+        this.colour = colour;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    private String colour;
+
+    @Override
+    public String toString() {
+        return "Vehicles{" +
+                "price=" + price +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
+}
